d46d4b6b6a3f8b3aa073628d49a61208695ac378
Yigermal Abebe <yigoya7@gmail.com>
2026-05-29 18:14:12 +0300

    chore: update environment paths and project dependencies

--- flutter_mobile/lib/core/config/app_config.dart
+++ flutter_mobile/lib/core/config/app_config.dart
@@ -1,32 +1,19 @@
 import 'package:flutter/foundation.dart';
 
-
-
 class AppConfig {
-
   const AppConfig._();
 
-
-
   static String get apiBaseUrl {
-
     if (kIsWeb) {
-
       return const String.fromEnvironment(
-
         'ATLAS_API_BASE_URL',
-
-        defaultValue: 'http://10.19.227.145:8080/api/v1',
-
+        defaultValue: 'http://10.100.205.105:8080/api/v1',
       );
-
     }
-
-
 
     return const String.fromEnvironment(
       'ATLAS_API_BASE_URL',
-      defaultValue: 'http://10.19.227.145:8080/api/v1',
+      defaultValue: 'http://10.100.205.105:8080/api/v1',
     );
   }
 }
